0580ffd4dc8196a05fc71de1f557056e6595ae59
kingkazuma7 <your.nasionasio7@gmail.com>
2024-04-23 11:51:09 +0900

    ReactでTypeScriptを使う⑦useStateの書き方 nullに空のオブジェクトをセットしてエラー回避

--- src/components/TypeScript/UseStateTSX.tsx
+++ src/components/TypeScript/UseStateTSX.tsx
@@ -1,21 +1,34 @@
 import React, { useState } from 'react'
 
+type User = {
+  name: string;
+  email: string;
+  tel: string;
+}
+
 const UseStateTSX = () => {
-  const [loginStatus, setLoginStatus] = useState(false)
+  // const [loginStatus, setLoginStatus] = useState(false)
+  const [user, setUser] = useState<User>({} as User);
+  console.log(user);
+  
 
   const isLogIn = () => {
-    setLoginStatus(true);
-  }
-  const isLogOut = () => {
-    setLoginStatus(false);
+    setUser({
+      name: 'yamada',
+      email: 'test@test.com',
+      tel: "",
+    })
   }
   // console.log(loginStatus);
   
   return (
     <div>
       <button onClick={isLogIn}>ログイン</button>
-      <button onClick={isLogOut}>ログアウト</button>
-      <p>ステータスは、{loginStatus ? "ログイン" : "ログアウト" }です。</p>
+      {/* <button onClick={isLogOut}>ログアウト</button> */}
+      {/* <p>ステータスは、{loginStatus ? "ログイン" : "ログアウト" }です。</p> */}
+      <p>ユーザー名：{user.name}</p>
+      <p>メールアドレス：{user.email}</p>
+      <p>電話：{user.tel}</p>
     </div>
   )
 }
